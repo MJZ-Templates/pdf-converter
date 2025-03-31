1829a5415570a29b5cb60b090d2bb024fbe6f76b
mango <mango.koo@goorm.io>
2025-03-31 18:31:12 +0900

    fix: remove unusing files

--- back/src/main/java/arkain/dev/back/common/web/ui/HealthCheckController.java
+++ back/src/main/java/arkain/dev/back/common/web/ui/HealthCheckController.java
@@ -1,5 +1,6 @@
 package arkain.dev.back.common.web.ui;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
     @GetMapping
-    public String healthCheck() {
-        return "ok";
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
     }
 }
